0a2e36cdfab05db5e2ce09ea4504c49d6a6c07f8
Jack Milo <mil21001248@btc.ac.uk>
2024-06-18 10:30:25 +0100

    Move player with mouse.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(512, 512), "Simple Window", sf::Style::Close | sf::Style::Titlebar);
 	sf::RectangleShape player(sf::Vector2f(100.f, 100.f));
 	player.setFillColor(sf::Color::Red);
+	player.setOrigin(50.0f, 50.0f);
 
 	while (window.isOpen())
 	{
@@ -17,21 +18,10 @@ int main()
 				window.close();
 			}
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			player.move(0.0f, -0.1f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-		{
-			player.move(0.0f, 0.1f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-		{
-			player.move(-0.1f, 0.0f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-		{
-			player.move(0.1f, 0.0f);
+			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+			player.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
 		}
 
 		window.clear();
