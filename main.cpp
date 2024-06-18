c70e7907f883f64d0c92abaaa164da867c2abb5a
Jack Milo <mil21001248@btc.ac.uk>
2024-06-18 10:04:46 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(512, 512), "Simple Window", sf::Style::Close | sf::Style::Titlebar);
+	
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+
+		while (window.pollEvent(evnt))
+		{
+			if (evnt.type == evnt.Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
+	return 0;
+}
